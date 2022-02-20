a8222ef5089ad4dc4b8651abc3f3f8ecb09b5d4e
SergeichNik <tyunev.nik89@gmail.com>
2022-02-20 23:42:25 +0300

    newEmpAdd logic add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 import AppInfo from "./component/AppInfo";
 import {SearchInput} from "./component/SearchInput/SearchInput";
 import {EmpItemsList} from "./component/EmpItemsList/EmpItemsList";
 import {EmpAddInput} from "./component/EmpAddInput/EmpAddInput";
+import { v1 } from 'uuid';
 
 export type IncomingDataType = {
     id: string,
@@ -14,19 +15,49 @@ export type IncomingDataType = {
     increase: boolean
 }
 
+ export type UserPropType = {
+    name: string,
+    salary: string
+}
+
 function App() {
     const incomingData = [
-        {id: "1", name: 'Alex V.', salary: 2000, promo: false, increase: false},
-        {id: "2", name: 'Alex V.', salary: 2000, promo: false, increase: false},
-        {id: "3", name: 'Alex V.', salary: 2000, promo: false, increase: false},
+        {id: v1(), name: 'Alex V.', salary: 2000, promo: false, increase: false},
+        {id: v1(), name: 'Alex V.', salary: 2000, promo: false, increase: false},
+        {id: v1(), name: 'Alex V.', salary: 2000, promo: false, increase: false},
     ]
+    const [data, setData] = useState(incomingData)
+    const [userProp, setUserProp] = useState<UserPropType>({name: '', salary: ''})
+
+    const onAddNewEmp = () => {
+        if (userProp.name === '' || userProp.salary === '') {
+            return
+        } else {
+            let newEmp = {
+                id: v1(),
+                name: userProp.name,
+                salary: +userProp.salary,
+                promo: false,
+                increase: false
+            }
+            setData([...data, newEmp]);
+            setUserProp({...userProp, name: '', salary: ''})
+        }
+
+    }
+
+    const onChangeUserProp = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let {name, value} = e.currentTarget
+        setUserProp({...userProp, [name]: value});
+    }
+
 
     return (
         <div className="App">
             <AppInfo/>
             <SearchInput/>
-            <EmpItemsList data={incomingData}/>
-            <EmpAddInput/>
+            <EmpItemsList data={data}/>
+            <EmpAddInput onAddNewEmp={onAddNewEmp} userProp={userProp} setUserProp={onChangeUserProp}/>
         </div>
     );
 }
--- src/component/EmpAddInput/EmpAddInput.tsx
+++ src/component/EmpAddInput/EmpAddInput.tsx
@@ -1,12 +1,29 @@
 import classes from "./EmpAddInput.module.css";
+import React from "react";
+import { UserPropType } from "../../App";
+
+type PropsType = {
+    userProp: UserPropType,
+    setUserProp : (e: React.ChangeEvent<HTMLInputElement>) => void
+    onAddNewEmp: () => void
+}
+
+export const EmpAddInput: React.FC<PropsType> = ({userProp, setUserProp, onAddNewEmp}) => {
 
 
-export const EmpAddInput = () => {
     return (
         <div className={classes.footer}>
-            <input placeholder={'Enter name . . .'}/>
-            <input placeholder={'Enter salary . . .'}/>
-            <button>Add</button>
+            <input placeholder={'Enter name . . .'}
+                   value={userProp.name}
+                   onChange={setUserProp}
+                   name={'name'}
+            />
+            <input placeholder={'Enter salary . . .'}
+                   value={userProp.salary}
+                   onChange={setUserProp}
+                   name={'salary'}
+            />
+            <button onClick={onAddNewEmp} >Add</button>
         </div>
     )
 }
